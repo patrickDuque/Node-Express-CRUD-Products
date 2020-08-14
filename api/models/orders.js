ead8eb7c7224951ed5bea70a0a7ed18f3825f846
Patrick Duque <patpatduque26@gmail.com>
2020-08-14 12:43:41 +0800

    added orders schema and route

--- api/models/orders.js
+++ api/models/orders.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
-  product  : { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  product  : { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: [ true, 'Product required' ] },
   quantity : { type: Number, default: 1 }
 });
 
-module.exports = mongoose.model('Orders', orderSchema);
+module.exports = mongoose.model('Order', orderSchema);
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,14 +1,71 @@
 const express = require('express');
 
+const Order = require('../models/orders');
+const Product = require('../models/products');
+const { findById } = require('../models/products');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'products' });
+router.get('/', async (req, res) => {
+  try {
+    const orders = await Order.find().populate('product').select('product _id quantity productName');
+    res.status(200).json({ orders });
+  } catch (error) {
+    res.status(500).json({ error: { message: 'Error fetching orders', error: error.message } });
+  }
 });
 
-router.get('/:orderId', (req, res) => {
-  const id = req.params.orderId;
-  res.status(200).json({ message: `order ${id}` });
+router.post('/', async (req, res) => {
+  try {
+    const { productId, quantity } = req.body;
+    const product = await Product.findById(productId);
+    if (product) {
+      const order = new Order({
+        quantity : +quantity,
+        product  : productId
+      });
+      const newOrder = await order.save();
+      res.status(201).json({ message: 'Order added', order: newOrder });
+    } else {
+      res.status(404).json({ message: `No product with product id ${productid}` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: { message: 'Error adding order', error: error.message } });
+  }
+});
+
+router.get('/:orderId', async (req, res) => {
+  try {
+    const id = req.params.orderId;
+    const order = await Order.findById(id).populate('product').select('product _id quantity');
+    if (order) {
+      return res.status(200).json({ order });
+    } else {
+      return res.status(404).json({ message: 'No order found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: { message: 'Error fetching order', error: error.message } });
+  }
+});
+
+router.delete('/:orderId', async (req, res) => {
+  try {
+    const id = req.params.orderId;
+    const order = await Order.findByIdAndDelete(id);
+    res.status(201).json({ message: 'Deleted order', order });
+  } catch (error) {
+    res.status(500).json({ error: { message: 'Error deleting order', error: error.message } });
+  }
+});
+
+router.put('/:orderId', async (req, res) => {
+  try {
+    const id = req.params.orderId;
+    const order = await Order.findByIdAndUpdate(id, { quantity: req.body.quantity });
+    res.status(201).json({ message: 'Edited order', order });
+  } catch (error) {
+    res.status(500).json({ error: { message: 'Error deleting order', error: error.message } });
+  }
 });
 
 module.exports = router;
